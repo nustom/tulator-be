96cc7ec63775b7f1fc341a4dbd6d424c4bfdbf7f
nustom <tom@nustechnology.com>
2022-05-25 17:04:39 +0700

    Connect server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,22 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { ServerInterface } from './app.interface';
 import { connectToDatabase } from '../config/db';
 import baseRouter from '../modules/baseRouter';
-class Server implements ServerInterface {// eslint-disable-line
-  constructor() {
-    connectToDatabase()
-  }
-
+class Server implements ServerInterface {
   async server(): Promise<express.Application> {
+    await connectToDatabase();
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors());
-    app.use('/api/v1', baseRouter.routes);//setting up base route
-    // define a route handler for the default home page
-    app.get("/", (_req, res) => {
-      res.send("Welcome to express-create application! ");
+    app.use('/api/v1', baseRouter.routes);
+
+    app.get("/", (_req: Request, res: Response): Response => {
+      return res.send("Welcome to express-create application! ");
     });
 
     return app;
